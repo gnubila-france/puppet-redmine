7a304203214bf2baa32581694afe778b3e8598d1
Baptiste Grenier <bgrenier@gnubila.fr>
2014-07-31 16:15:16 +0200

    Install some dependencies.

--- manifests/dependencies.pp
+++ manifests/dependencies.pp
@@ -1,7 +1,12 @@
 class redmine::dependencies {
-  # TODO install dependencies
-  # - ruby
-  # - imagemagick?
+  package { 'imagemagick':
+    ensure => $redmine::manage_package,
+  }
+  if $redmine::db_type == 'mysql' {
+    package { 'ruby-mysql2':
+      ensure => $redmine::manage_package,
+    }
+  }
 }
 
 # vim: set et sw=2:
