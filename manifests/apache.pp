2e0a38be994d530615bae954b1b88c96be15f489
Sean Conley <sconley@caci.com>
2016-06-22 14:38:50 -0600

    added option for hiera provided ssl data

--- manifests/apache.pp
+++ manifests/apache.pp
@@ -38,52 +38,106 @@ class redmine::apache {
 
   include ::redmine
   include ::apache
+  include ::apache::mod::passenger
 
-  # where is $template_passenger being used ???
-  #$template_passenger = $redmine::apache::template_passenger
-  
   if $::redmine::ssl {
+
     include ::apache::mod::ssl
 
-    file { $::redmine::ssl_cert:
-      ensure  => 'file',
-      owner   => $::apache::user,
-      group   => $::apache::group,
-      mode    => '0640',
-      source  => $::redmine::ssl_cert_src,
-      notify  => Class['::apache::service'],
-      require => [
-        File[$::redmine::ssl_cert_key],
-        File[$::redmine::ssl_ca_cert],
-      ]
+    # it seems that there should be a better way to choose between 'content' and 'source' attributes.
+    # any suggestions?
+    if  $::redmine::ssl_cert_content != 'undef'  {
+      file { $::redmine::ssl_cert:
+        ensure  => 'file',
+        owner   => $::apache::user,
+        group   => $::apache::group,
+        mode    => '0640',
+        notify  => Class['::apache::service'],
+        content => $::redmine::ssl_cert_content,
+        require => [
+          File[$::redmine::ssl_cert_key],
+          File[$::redmine::ssl_ca_cert],
+        ]
+      }
+    } else {
+      file { $::redmine::ssl_cert:
+        ensure  => 'file',
+        owner   => $::apache::user,
+        group   => $::apache::group,
+        mode    => '0640',
+        notify  => Class['::apache::service'],
+        source  => $::redmine::ssl_cert_src,
+        require => [
+          File[$::redmine::ssl_cert_key],
+          File[$::redmine::ssl_ca_cert],
+        ]
+      }
     }
-    file { $::redmine::ssl_cert_key:
-      ensure => 'file',
-      owner  => $::apache::user,
-      group  => $::apache::group,
-      mode   => '0400',
-      source => $::redmine::ssl_cert_key_src,
-      notify => Class['::apache::service'],
-    }
-    if ! defined(File[$::redmine::ssl_ca_cert]) {
-      file { $::redmine::ssl_ca_cert:
+
+
+    if  $::redmine::ssl_cert_key_content != 'undef'  {
+      file { $::redmine::ssl_cert_key:
         ensure => 'file',
         owner  => $::apache::user,
         group  => $::apache::group,
-        mode   => '0640',
-        source => $::redmine::ssl_ca_cert_src,
+        mode   => '0400',
+        content => $::redmine::ssl_cert_key_content,
+        notify => Class['::apache::service'],
+      }
+    } else {
+      file { $::redmine::ssl_cert_key:
+        ensure => 'file',
+        owner  => $::apache::user,
+        group  => $::apache::group,
+        mode   => '0400',
+        source => $::redmine::ssl_cert_key_src,
         notify => Class['::apache::service'],
       }
     }
+
+    if ! defined(File[$::redmine::ssl_ca_cert]) {
+      if  $::redmine::ssl_ca_cert_content != 'undef'  {
+        file { $::redmine::ssl_ca_cert:
+          ensure  => 'file',
+          owner   => $::apache::user,
+          group   => $::apache::group,
+          mode    => '0640',
+          content => $::redmine::ssl_ca_cert_content,
+          notify  => Class['::apache::service'],
+        }
+      } else {
+        file { $::redmine::ssl_ca_cert:
+          ensure => 'file',
+          owner  => $::apache::user,
+          group  => $::apache::group,
+          mode   => '0640',
+          source => $::redmine::ssl_ca_cert_src,
+          notify => Class['::apache::service'],
+        }
+      }
+    }
+
     if $::redmine::ssl_ca_cert_chain != undef and
       ! defined(File[$::redmine::ssl_ca_cert_chain]) {
-      file { $::redmine::ssl_ca_cert_chain:
-        ensure => 'file',
-        owner  => $::apache::user,
-        group  => $::apache::group,
-        mode   => '0640',
-        source => $::redmine::ssl_ca_cert_chain_src,
-        notify => Class['::apache::service'],
+
+      if  $::redmine::ssl_ca_cert_content != 'undef'  {
+        file { $::redmine::ssl_ca_cert_chain:
+          ensure => 'file',
+          owner  => $::apache::user,
+          group  => $::apache::group,
+          mode   => '0640',
+          content => $::redmine::ssl_ca_cert_chain_content,
+          notify => Class['::apache::service'],
+        }
+      } else {
+        file { $::redmine::ssl_ca_cert_chain:
+          ensure => 'file',
+          owner  => $::apache::user,
+          group  => $::apache::group,
+          mode   => '0640',
+          source => $::redmine::ssl_ca_cert_chain_src,
+          notify => Class['::apache::service'],
+        }
       }
     }
   }
@@ -95,7 +149,7 @@ class redmine::apache {
 
   $docroot = "${redmine::install_dir}/public/"
 
-  apache::vhost { "port80.${::redmine::server_name}":
+  apache::vhost { "${::redmine::server_name}-redirect":
     servername           => "${::redmine::server_name}",
     port                 => '80',
     docroot              => '/var/www/redirect',
@@ -103,28 +157,27 @@ class redmine::apache {
     redirect_dest        => "https://${::redmine::server_name}/",
   }
 
-  apache::vhost { "port443.${::redmine::server_name}":
+  apache::vhost { "${::redmine::server_name}-SSL":
     servername           => "${::redmine::server_name}",
     port                 => '443',
     serveraliases        => $::redmine::serveraliases,
     docroot              => $docroot,
     directories          => [
       {
-        path     => $docroot,
-        provider => 'directory',
-        order    => 'allow,deny',
-        allow    => 'from all',
-        options  => ['Indexes','ExecCGI','FollowSymLinks'],
-        override => ['All'],
+        path              => $docroot,
+        provider          => 'directory',
+        order             => 'allow,deny',
+        allow             => 'from all',
+        options           => ['Indexes','ExecCGI','FollowSymLinks'],
+        override          => ['All'],
+        passenger_enabled => 'on',
       },
     ],
-    #custom_fragment      => $custom_fragment,
-    #ssl                  => true,
     ssl                  => $::redmine::ssl,
     ssl_cert             => $::redmine::ssl_cert,
     ssl_key              => $::redmine::ssl_cert_key,
-    ssl_chain            => $::redmine::ssl_ca_cert_chain,
-    ssl_ca               => $::redmine::ssl_ca_cert,
+    #ssl_chain            => $::redmine::ssl_ca_cert_chain,
+    #ssl_ca               => $::redmine::ssl_ca_cert,
     ssl_protocol         => $::redmine::ssl_protocol,
     ssl_cipher           => $::redmine::ssl_cipher_suite,
     ssl_honorcipherorder => 'On',
