8928c4e37d5fcb35ce76d98f3e655109ef561bd2
Sean Conley <sconley@caci.com>
2016-07-05 23:45:16 -0600

    conversion to use plugin archive files vs git repos

--- manifests/plugin.pp
+++ manifests/plugin.pp
@@ -21,25 +21,42 @@
 # Copyright 2015 gnúbila
 #
 define redmine::plugin (
-  $user = $redmine::user,
-  $group = $redmine::group,
-  $repo_url = undef,
-  $revision = 'master',
-  $provider = 'git',
+  String $version,
+  String $plugin_repo = $redmine::plugin_repo,
+  String $plugin_repo_creds = $redmine::plugin_repo_creds,
+  String $plugin_repo_proto = $redmine::plugin_repo_proto,
+  String $user = $redmine::user,
+  String $group = $redmine::group,
+  #String $repo_url,
+  #String $revision,
+  #String $provider,
 ) {
   include ::redmine
 
-  if $repo_url == undef {
-    fail('Please provide rep_url.')
-  }
-  vcsrepo { "${redmine::install_dir}/plugins/${title}":
-    ensure   => 'present',
-    provider => $provider,
-    source   => $repo_url,
-    revision => $revision,
-    user     => $user,
-    notify   => Exec["Update gems environment using bundler for plugin ${title}"],
-    require  => File['redmine_link'],
+#  if $repo_url == undef {
+#    fail('Please provide rep_url.')
+#  }
+#
+#  vcsrepo { "${redmine::install_dir}/plugins/${title}":
+#    ensure   => 'present',
+#    provider => $provider,
+#    source   => $repo_url,
+#    revision => $revision,
+#    user     => $user,
+#    notify   => Exec["Update gems environment using bundler for plugin ${title}"],
+#    require  => File['redmine_link'],
+#  }
+
+
+  puppi::netinstall { $title:
+    url             => "${redmine::plugin_repo_proto}://${redmine::plugin_repo_creds}@${redmine::plugin_repo}/${title}/$version/${title}-${version}.zip",
+    destination_dir => "${redmine::user_home}/redmine-${redmine::version}/plugins/",
+    extracted_dir   => "$title",
+    #destination_dir => "${redmine::user_home}/redmine-${redmine::version}/plugins/extract_$title",
+    #postextract_command => "mv ${redmine::user_home}/redmine-${redmine::version}/plugins/extract_$title ${redmine::user_home}/redmine-${redmine::version}/plugins/$title", 
+    owner           => $user,
+    group           => $group,
+    #require         => 'redmine::redmine-database.conf',
   }
 
   $path = [
@@ -74,6 +91,8 @@ define redmine::plugin (
     refreshonly => true,
     require     => Exec['Run database migration'],
   }
+
+
 }
 
 # vim: set et sw=2:
