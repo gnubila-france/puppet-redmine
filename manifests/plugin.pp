51abd95cbc82986a7d5dbbd83fdfc0522246789a
root <root@redmine-tnc.geointservices.io>
2016-07-07 21:06:07 -0600

    include required gem env - still needs cleanup

--- manifests/plugin.pp
+++ manifests/plugin.pp
@@ -27,6 +27,7 @@ define redmine::plugin (
   String $plugin_repo_proto = $redmine::plugin_repo_proto,
   String $user = $redmine::user,
   String $group = $redmine::group,
+  String $bundle_without = $redmine::bundle_without,
   #String $repo_url,
   #String $revision,
   #String $provider,
@@ -52,44 +53,71 @@ define redmine::plugin (
     url             => "${redmine::plugin_repo_proto}://${redmine::plugin_repo_creds}@${redmine::plugin_repo}/${title}/$version/${title}-${version}.zip",
     destination_dir => "${redmine::user_home}/redmine-${redmine::version}/plugins/",
     extracted_dir   => "$title",
-    #destination_dir => "${redmine::user_home}/redmine-${redmine::version}/plugins/extract_$title",
-    #postextract_command => "mv ${redmine::user_home}/redmine-${redmine::version}/plugins/extract_$title ${redmine::user_home}/redmine-${redmine::version}/plugins/$title", 
     owner           => $user,
     group           => $group,
+    notify          => Exec["Install gems using bundler for plugin ${title}"],
     #require         => 'redmine::redmine-database.conf',
   }
 
   $path = [
-    "${redmine::user_home}/bin",
+    "${redmine::user_home}/bin", 
     '/bin', '/usr/bin', '/usr/sbin'
   ]
-  exec { "Update gems environment using bundler for plugin ${title}":
-    command     => 'bundle update',
-    user        => $user,
-    cwd         => "${redmine::install_dir}/plugins/${title}",
-    path        => $path,
-    refreshonly => true,
-    notify      => Exec["Install gems using bundler for plugin ${title}"],
-    require     => Exec['Install gems using bundler'],
-  }
+
+#  exec { "Update gems environment using bundler for plugin ${title}":
+#    command     => 'bundle update',
+#    user        => $user,
+#    cwd         => "${redmine::install_dir}/plugins/${title}",
+#    path        => $path,
+#    refreshonly => true,
+#    notify      => Exec["Install gems using bundler for plugin ${title}"],
+#    require     => Exec['Install gems using bundler'],
+#  }
+    $gemenv = [
+      "HOME=$redmine::user_home",
+      "BUNDLE_WITHOUT=--without xapian",
+      #"BUNDLE_WITHOUT=--without $bundle_without",
+      "RAILS_ENV=production",
+      "RACK_ENV=production",
+      #"DB_ADAPTER=mysql2",
+      #"GEM_OPTIONS=--no-rdoc --no-ri"
+    ]
+
   exec { "Install gems using bundler for plugin ${title}":
-    command     => 'bundle install --without development test',
+    #command     => 'bundle install --without development test',
+    # TEMP - need to pass this in
+    #command     => 'bundle install --without development:test:xapian',
+    #command     => 'bundle install --path ${redmine::user_home}/.gem --without development:test:xapian',
+    command     => 'bundle install ${BUNDLE_WITHOUT}',
     user        => $user,
-    cwd         => "${redmine::install_dir}/plugins/${title}",
+    #cwd         => "${redmine::install_dir}/plugins/${title}",
+    cwd         => "${redmine::install_dir}",
     path        => $path,
+    environment => $gemenv,
     refreshonly => true,
-    notify      => Exec["Run database migration for plugin ${title}"],
-    require     => Exec['Install gems using bundler'],
+    notify      => Exec["update db schema for plugin ${title}"],
+    #require     => Exec["Update gems environment using bundler for plugin ${title}"],
   }
 
-  exec { "Run database migration for plugin ${title}":
+  exec { "update db schema for plugin ${title}":
+    command     => 'bundle exec rake db:migrate',
+    user        => $user,
+    cwd         => "${redmine::install_dir}/plugins",
+    path        => $path,
+    environment => $gemenv,
+    refreshonly => true,
+    notify      => Exec["Run plugin migration for plugin ${title}"],
+    require     => Exec["Install gems using bundler for plugin ${title}"],
+  }
+
+  exec { "Run plugin migration for plugin ${title}":
     command     => 'bundle exec rake redmine:plugins:migrate',
     user        => $user,
     cwd         => $redmine::install_dir,
     path        => $path,
-    environment => [ 'RAILS_ENV=production' ],
+    environment => $gemenv,
     refreshonly => true,
-    require     => Exec['Run database migration'],
+    require     => Exec["update db schema for plugin ${title}"],
   }
 
 
