c44ce7f6f57450dca3e50c0c101e97f273d0e81d
Scott D. Howell <showell@solidyn.com>
2017-01-17 14:18:11 -0700

    Update to address idempotency issues

--- manifests/init.pp
+++ manifests/init.pp
@@ -278,6 +278,7 @@ class redmine (
     mode    => $redmine::config_file_mode,
     owner   => $redmine::config_file_owner,
     group   => $redmine::config_file_group,
+    seluser => 'system_u',
     require => File['redmine_link'],
     content => $redmine::manage_db_file_content,
     replace => $redmine::manage_file_replace,
